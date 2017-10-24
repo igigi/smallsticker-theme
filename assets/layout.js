eaec88c4ac9fa8134254b9b29582edf507414cf4
wu zhenyu <wuyuit@icloud.com>
2017-10-24 17:49:24 +0800

    product detail tab and cart api request

--- assets/layout.js
+++ assets/layout.js
@@ -1,17 +1,34 @@
-$(document)
-  .ready(function() {
+$.fn.api.settings.api = {
+  'show cart' : 'http://localhost:3000/carts/{id}',
+  'create cart'   : 'http://localhost:3000/cart',
+};
+$(document).api({
+    action: 'show cart',
+    urlData: {
+      id: 1
+    }
+  });
 
+$(document).ready(function() {
     // fix main menu to page on passing
     $('.main.menu').visibility({
       type: 'fixed'
     });
+
     $('.overlay').visibility({
       type: 'fixed',
       offset: 80
     });
+    $('.wechat-box').click(function(){
+      $('.mini.wechat-box.modal').modal('show');
+    });
     $('.gift-box').click(function(){
       $('.fullscreen.modal').modal('show');
     });
+    $('.menu .item')
+      .tab()
+    ;
+
     // lazy load images
     $('.image').visibility({
       type: 'image',
