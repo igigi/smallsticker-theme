2c1f916ec43f8bc9dd86b7493354e5425787184e
wu zhenyu <wuyuit@icloud.com>
2017-10-27 10:25:30 +0800

    ui complate

--- assets/layout.js
+++ assets/layout.js
@@ -1,30 +1,19 @@
-$.fn.api.settings.api = {
-  'show cart' : 'http://localhost:3000/carts/{id}',
-  'create cart'   : 'http://localhost:3000/cart',
-};
-$(document).api({
-    action: 'show cart',
-    urlData: {
-      id: 1
-    }
-  });
+$(document)
+  .ready(function() {
 
-$(document).ready(function() {
     // fix main menu to page on passing
     $('.main.menu').visibility({
       type: 'fixed'
     });
 
-    $('.overlay').visibility({
-      type: 'fixed',
-      offset: 80
-    });
     $('.wechat-box').click(function(){
       $('.mini.wechat-box.modal').modal('show');
     });
+
     $('.gift-box').click(function(){
       $('.fullscreen.modal').modal('show');
     });
+
     $('.menu .item')
       .tab()
     ;
